d3d17f1173d74fbcff3faf5a5071dab5b967deb9
zap-rowsdower <brock.whitbread@gmail.com>
2018-05-30 09:40:19 -0700

    Moves the constructor function and toggleHidden function to FeatureNode

--- src/index.js
+++ src/index.js
@@ -5,17 +5,7 @@ import campFeatures from './camp_features'
 
 // Main container component for the feature list
 class FeatureList extends React.Component {
-    constructor () {
-        super()
-        this.state = {
-            isHidden: true
-        }
-    }
-    toggleHidden () {
-        this.setState({
-            isHidden: !this.state.isHidden
-        })
-    }
+
     render () {
         // This loops through our campFeatures and creates a new component for each one, passing along the main feature and its subfeatures as props 
         let nodes = campFeatures.map((feature) => 
@@ -32,12 +22,25 @@ class FeatureList extends React.Component {
 
 // FeatureNode component outputs the individual feature as a list item and is also where recursion takes place
 class FeatureNode extends React.Component {
+    constructor () {
+        super()
+        this.state = {
+            isHidden: true
+        }
+    }
+
+    toggleHidden () {
+        this.setState({
+            isHidden: !this.state.isHidden
+        })
+    }
+
     render () {
 
         let childnodes = null;
 
         // if children exist, this component will call itself
-        if (this.props.children) {
+        if (this.props.children.length > 0) {
             childnodes = this.props.children.map((childnode) => 
                 <FeatureNode node={childnode} children={childnode.subfeatures} />
             );
@@ -45,10 +48,10 @@ class FeatureNode extends React.Component {
 
         // return the feature and the subfeatures if they exist
         return (
-            <li key={this.props.node.id}>
+            <li key={this.props.node.id} onClick={this.toggleHidden.bind(this)}>
                 <span>{this.props.node.title}</span>
                 { childnodes ? (
-                    <ul>{childnodes}</ul>
+                    <ul>{!this.state.isHidden && childnodes}</ul>
                 ) : (null) }
             </li>
         );
